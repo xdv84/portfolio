b1c15ca7abe1b67e058c4002c09177a351f1b1cb
xdv84 <starburg1405@gmail.com>
2022-04-21 17:57:30 +0300

    second commit

---
+++ main.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     console.log('All right')
+// })
+
+// let FirstName = 'Dmitrii';
+// console.log( `My name is ${ FirstName }` )
+
+// console.log(Math.PI);
+
+// let myName = 'Ivan'
+// myName = 'Dima'
+// console.log(myName)
+
+// var secondName = 'Tonya'
+// console.log(secondName) 
+
+// let obj = {
+//     a: 10,
+//     b: 20,
+//     c: 30
+// }
+
+// console.log(obj['c'])
+// console.log(obj.c)
+// console.log(obj)
+
+// let arr = ['dima', 'tonya', 37, 38, 'family']
+// console.log(arr)
+// console.log(arr[4], arr[3], arr[0])
+
+// let one = 'two'
+// console.log(typeof one) 
+
+// let category = 'toys'
+// console.log('https://google.com/' + category)
+// console.log(`https://google.com/${category}/5`)
+
+// let incr = 10,
+//     decr = 20;
+
+// console.log(incr++)
+// console.log(decr--)
+// // console.log(++incr)
+// // console.log(--decr)
+// console.log(incr)
+
+console.log(2 + 2 * 2 !== '6')
