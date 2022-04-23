820da192a73df7b7d45857bec7039d62714c4246
Dmitrii X <starburg1405@gmail.com>
2022-04-23 18:04:36 +0300

    method

--- main.js
+++ main.js
@@ -202,10 +202,103 @@ str.length - и получить её длину)
 // let summ = (a, b) => a + b
 // console.log(summ(4, 5))
 
-//Методы и свойства строк и чисел
-let string = 'DIMA'
-let str = 'dima'
-console.log(string.length) // свойство
-console.log(string.toLowerCase()) // метод
-console.log(str.toUpperCase()) // метод
+// //Методы и свойства строк и чисел
+// let string = 'DIMA'
+// let str = 'dima'
+// console.log(string.length) // свойство
+// console.log(string.toLowerCase()) // метод
+// console.log(str.toUpperCase()) // метод
 
+// let fruit = "Apple Orange"
+// console.log(fruit.indexOf('Orange')) // обращаемся к строке и выводим порядковый номер, в данном случае Orange это 6-ка
+
+// let logg = 'Hello World'
+// console.log(logg.slice(6, 11)) //метод slice выдергиавает из контекста, первый аргемент с какого симола, второй аргумент по какой + 1, можно без 2-го аргумента, возможно использовать отрицательные аргементы, чтобы выдергивать с конца контекста
+
+// console.log(logg.substring(6, 11)) // аналог slice, но нельзя использовать отрицательные значения
+
+// console.log(logg.substr(6, 5)) // метод позволяет верезать конкретное кол-во символом вторым аргементом, 1 аргемент с какого, 2 аргемент сколько 
+
+// let num = 36.6
+// console.log(Math.round(num))
+
+// let str = '12.2 px'
+// console.log(parseInt(str)) // метод parseInt преобразует строку в число и округляет; parseFloat не округляет
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+//1
+let numberOfFilms
+
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+    }
+}
+start()
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+function rememberMyFilms () {
+    for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+}
+rememberMyFilms()
+
+function detectPersonalLevel () {
+        if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+detectPersonalLevel()
+
+//2
+function showMyDb(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB)
+    }
+}
+showMyDb(personalMovieDB.privat)
+
+//3
+
+function writeYourGenres() {
+    for (let i=1; i<=3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`)
+    }
+}
+writeYourGenres()
