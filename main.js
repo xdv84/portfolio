abacfdef99f0dd4e0193de45029ff7858333a2ac
Dmitrii X <starburg1405@gmail.com>
2022-05-14 00:24:07 +0300

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 'use strict'
 
+// let num = 50
+//
+// while (num <= 100) {
+//   console.log(num)
+//   num++
+// }
+//
+//
+// let number = 100
+//
+// do {
+//   console.log(number)
+//   number++
+// } while (number <= 150)
 
-alert( "\u{1F60D}" )
+
+
+for (let i = 0; i <= 10; i++) {
+  if (i === 6) {
+    break
+  }
+  console.log(i)
+}
+
