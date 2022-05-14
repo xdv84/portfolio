a0618c3c532e6beadfc327e0293aea0726fbfb03
Dmitrii X <starburg1405@gmail.com>
2022-05-15 00:00:55 +0300

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 'use strict'
 
+// ******************************************___ОБЪЕКТЫ___******************************************
+
+
+const user = {
+  name: 'Dima',
+  age: 38,
+  city: 'Bansko'
+}
+
+
+
+// ******************************************___ЦИКЛЫ___******************************************
+
+
 // let num = 50
 //
 // while (num <= 100) {
@@ -15,12 +29,42 @@
 //   number++
 // } while (number <= 150)
 
+// for (let i = 0; i <= 10; i++) {
+//   if (i === 6) {
+//     break
+//   }
+//   console.log(i)
+// }
 
 
-for (let i = 0; i <= 10; i++) {
-  if (i === 6) {
-    break
-  }
-  console.log(i)
-}
+// ******************************************___ФУНКЦИИ___******************************************
+
+// Function declaration
+
+// let num = 50
+//
+// function getFirstNumber(text) {
+//   console.log(text)
+//   num = 20
+// }
+//
+// getFirstNumber('Hello World')
+//
+// console.log(num)
+
+// Function expression
+
+// const logger = function (a, b) {
+//   return a + b
+// }
+//
+// console.log(logger(3, 4))
+
+// Стрелочная функция () =>
+
+// const number = () => console.log('Стрелочная функция')
+// number()
+//
+// const sumNum = (a, b) => a + b
+// console.log(sumNum(3,4))
 
